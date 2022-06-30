6613fb5ae0ad02525c7214442e25981efd760980
amitir22 <amitir22@gmail.com>
2022-07-01 00:04:40 +0300

    updated py tester

--- compile_and_run.py
+++ compile_and_run.py
@@ -20,6 +20,7 @@ stdout, stderr = make_process.communicate()
 if stderr is not None:
     print(f"{RED}BUILD FAILED!{NC}")
     exit(0)
+print()
 
 # Run tests
 total_tests_count = 0
@@ -57,8 +58,8 @@ for root, _, _ in os.walk(f"{MAIN_TESTS_FOLDER}"):
         fanC_file = open(test_in)
         llvm_file = open(llvm_filename, 'w')
 
-        compile_process = subprocess.Popen("./hw5", stdin=fanC_file, stdout=llvm_file)
-        compile_process.communicate()
+        compile_process = subprocess.Popen("./hw5", stdin=fanC_file, stdout=llvm_file, stderr=subprocess.PIPE)
+        _, compile_stderr = compile_process.communicate()
 
         fanC_file.close()
         llvm_file.close()
@@ -68,26 +69,33 @@ for root, _, _ in os.walk(f"{MAIN_TESTS_FOLDER}"):
         llvm_file = open(llvm_filename)
         fanC_out_file = open(fanC_out_filename, 'w')
 
-        llvm_process = subprocess.Popen("lli", stdin=llvm_file, stdout=fanC_out_file)
-        llvm_process.communicate()
+        llvm_process = subprocess.Popen("lli", stdin=llvm_file, stdout=fanC_out_file, stderr=subprocess.PIPE)
+        _, llvm_stderr = llvm_process.communicate()
 
         llvm_file.close()
         fanC_out_file.close()
 
         # check if there are differences between the test.our.out to the test.out
         diff_process = subprocess.Popen(["diff", "--minimal", fanC_out_filename, test_out], stdout=subprocess.PIPE)
-        stdout, _ = diff_process.communicate()
+        diff_stdout, _ = diff_process.communicate()
 
-        if len(stdout) == 0:
+        if len(diff_stdout) == 0:
             successful_tests_count += 1
             files_to_remove = glob.glob(f"{root}/{test_in_basename.replace('.in', '.our.*')}")
             for file in files_to_remove:
                 os.remove(file)
+            clear_row()
         else:
             failed_tests_count += 1
             failed_tests.append(test_in_filename)
+            if len(compile_stderr):
+                print(compile_stderr.decode())
+            elif len(llvm_stderr):
+                print(llvm_stderr.decode())
+            else:
+                print(diff_stdout.decode())
 
-        clear_row()
+
 
 if failed_tests_count == 0:
     print(f"{GREEN}ALL TEST PASSED ({successful_tests_count}/{total_tests_count}){NC}")
